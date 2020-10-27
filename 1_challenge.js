8803373557f2943ffea2af964ff91c84a3b83ef9
Richard Leung <michelleli@MMac.local>
2020-10-27 10:43:30 -0400

    [1_challenge.js] Complete functions]

--- 1_challenge.js
+++ 1_challenge.js
@@ -3,7 +3,9 @@ Make a function createNewElement(tagName, text) that takes in 2
 parameters and returns a new element with the correct text
 ************************************************************** */
 function createNewElement(tagName, text) {
-    
+    let newElement = document.createElement(tagName);
+    newElement.textContent = text;
+    return newElement;
 }
 
 console.log(createNewElement('p', 'I am a software engineer'));
@@ -24,7 +26,17 @@ const instuctors = [
 ]
 
 const giveMeKeys = (array) => {
-
+    let keys = [];
+    // for (let i = 0; i < array.length; i++) {
+    //     const object = array[i];
+    //     for (const key in object) {
+    //         keys.push(key);
+    //     }
+    // }
+    array.forEach(object => {
+        keys = keys.concat(Object.keys(object));
+    });
+    return keys;
 }
 
 console.log(giveMeKeys(instuctors));
